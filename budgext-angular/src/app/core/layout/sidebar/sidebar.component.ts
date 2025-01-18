d03f7392ac9938960aa2ba342047b076fb14cd41
Alexander Grotjahn <GrotjahnAL@googlemail.com>
2025-01-18 22:47:40 +0100

    Work on user layout's sidenav

--- budgext-angular/src/app/core/layout/sidebar/sidebar.component.ts
+++ budgext-angular/src/app/core/layout/sidebar/sidebar.component.ts
@@ -1,11 +1,28 @@
 import { Component } from '@angular/core';
+import { MatListItem, MatNavList } from '@angular/material/list';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [],
+  imports: [
+    MatNavList,
+    MatListItem,
+    RouterLink,
+    RouterLinkActive,
+  ],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss'
 })
 export class SidebarComponent {
+  LINK_LIST = [
+    {label: 'Dashboard', path: '/user/dashboard', isActive: true},
+    {label: 'Transactions', path: '/user/transactions', isActive: false},
+    {label: 'Budget', path: '/user/budget', isActive: false},
+    {label: 'User settings', path: '/user/settings', isActive: false},
+  ];
+  constructor() {}
 
+  trackByFn(index: number, link: any): string {
+    return link.path;
+  }
 }
--- budgext-angular/src/app/core/layout/user-layout/user-layout.component.ts
+++ budgext-angular/src/app/core/layout/user-layout/user-layout.component.ts
@@ -2,10 +2,18 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from "../header/header.component";
 import { RouterOutlet } from '@angular/router';
 import { SidebarComponent } from "../sidebar/sidebar.component";
+import { MatSidenav, MatSidenavContainer, MatSidenavContent } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-user-layout',
-  imports: [RouterOutlet, HeaderComponent, SidebarComponent],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    SidebarComponent,
+    MatSidenavContainer,
+    MatSidenav,
+    MatSidenavContent,
+  ],
   templateUrl: './user-layout.component.html',
   styleUrl: './user-layout.component.scss'
 })
