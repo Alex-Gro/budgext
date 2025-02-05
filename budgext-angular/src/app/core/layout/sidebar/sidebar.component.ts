769c333bd7238b0c04baf742ae56be6d8f9af0b9
Alexander Grotjahn <GrotjahnAL@googlemail.com>
2025-02-05 22:04:00 +0100

    Add user settings mat menu to sidebar

--- budgext-angular/src/app/core/layout/sidebar/sidebar.component.ts
+++ budgext-angular/src/app/core/layout/sidebar/sidebar.component.ts
@@ -1,10 +1,14 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
 import { MatListItem, MatListItemIcon, MatNavList } from '@angular/material/list';
-import { RouterLink, RouterLinkActive, RouterModule } from '@angular/router';
+import { ActivatedRoute, RouterLink, RouterLinkActive, RouterModule } from '@angular/router';
 import { MatIcon } from '@angular/material/icon';
 import { MatTooltip } from '@angular/material/tooltip';
 import { MatDividerModule } from '@angular/material/divider';
-import { MatIconButton } from '@angular/material/button';
+import { MatButton, MatIconButton } from '@angular/material/button';
+import { UserService } from '../../auth/services/user.service';
+import { Subject, takeUntil } from 'rxjs';
+import { User } from '../../auth/user.model';
+import { MatMenu, MatMenuItem, MatMenuTrigger } from '@angular/material/menu';
 
 @Component({
   selector: 'app-sidebar',
@@ -19,19 +23,44 @@ import { MatIconButton } from '@angular/material/button';
     MatTooltip,
     MatDividerModule,
     MatIconButton,
+    MatMenuTrigger,
+    MatMenu,
+    MatMenuItem,
+    MatButton,
   ],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss'
 })
-export class SidebarComponent {
+export class SidebarComponent implements OnInit, OnDestroy {
   LINK_LIST = [
     {label: 'Dashboard', path: '/user/dashboard', matIcon: 'dashboard'},
     {label: 'Transactions', path: '/user/transactions', matIcon: 'payments'},
     // {label: 'Budget', path: '/user/budget'},
     // {label: 'User settings', path: '/user/settings'},
   ];
+
+  private _ngUnsubscribe: Subject<void> = new Subject<void>();
+  public currentUser: User | null = null;
+
   @Input() isExpanded: boolean = true;
   @Output() toggleMenu = new EventEmitter();
-  constructor() {}
+  constructor(private userService: UserService,
+              private route: ActivatedRoute) {}
 
+  ngOnInit(): void {
+    this.userService.currentUser$.pipe(takeUntil(this._ngUnsubscribe)).subscribe((user) => {
+      if (user) {
+        this.currentUser = user;
+      }
+    });
+  }
+
+  logout(): void {
+    this.userService.logout();
+  }
+
+  ngOnDestroy(): void {
+    this._ngUnsubscribe.next();
+    this._ngUnsubscribe.complete();
+  }
 }
